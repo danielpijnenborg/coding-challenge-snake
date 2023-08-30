3c52c4f894b6c9f15387a8b8995b78f435d4aedb
danielpijnenborg <danielpijnenborg@gmail.com>
2023-08-30 16:27:27 +0200

    update testfile

--- test_bot.py
+++ test_bot.py
@@ -1,9 +1,9 @@
-from .bot import ExampleBot
+from .bot import Explorer
 
 
 def test_create():
     """
     Test if the bot can be created
     """
-    bot = ExampleBot(id=0, grid_size=(1, 1))
+    bot = Explorer(id=0, grid_size=(1, 1))
     assert bot is not None
