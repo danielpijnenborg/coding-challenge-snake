144b6e791ee978bb7fe472bee2d790a4892ecac4
danielpijnenborg <danielpijnenborg@gmail.com>
2023-09-02 15:05:02 +0200

    reduce computation time

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from ...bot import Bot
 from ...constants import Move, MOVE_VALUE_TO_DIRECTION
 from ...snake import Snake
 
+np.set_printoptions(precision=1)
 
 def is_on_grid(pos: np.array, grid_size: Tuple[int, int]) -> bool:
     """
@@ -27,30 +28,32 @@ def collides(pos: np.array, snakes: List[Snake]) -> bool:
 
 
 def create_grid (self, snake: Snake, other_snakes: List[Snake], candies: List[np.array]):
-    grid = [[0 for x in range(self.grid_size[0])] for y in range(self.grid_size[1])]
-    for x in range(self.grid_size[0]):
-        for y in range(self.grid_size[1]):
-            score = 0
-            for c in candies:
-                score = score + (100 / (1 + math.dist([x, y], c)))
-            other_snakes.append(snake)
-            if collides ([x,y], other_snakes):
-                score = -10
-            grid[x][y] = score
-            # print("%.1f" % score, end=" ")
-        # print (";")
-    # print(grid)
+    x = np.arange(self.grid_size[0])
+    y = self.grid_size[1]-1 - np.arange(self.grid_size[1])
+    nx = np.ones(self.grid_size[0])
+    ny = np.ones(self.grid_size[1])
+    xg = np.outer(ny, x)
+    yg = np.outer(y, nx)
+    scoregrid = np.zeros(self.grid_size)
+    for c in candies:
+        scoregrid = scoregrid + (100 / (1 +(xg-c[0])**2+(yg-c[1])**2))
+    other_snakes.append(snake)
+    for snake in other_snakes:
+        print (snake)
+        for p in snake.positions:
+            scoregrid[self.grid_size[1]-1-p[1]][p[0]] = -10
+    # print (scoregrid)
     max_move = []
-    score = -1;
+    score = -9;
     for move in MOVE_VALUE_TO_DIRECTION:
         target = snake[0] + MOVE_VALUE_TO_DIRECTION[move]
         if is_on_grid(target, self.grid_size):
             # print (move)
             # print(target)
-            # print ("score: %.1f" %  grid[target[0]][target[1]])
-            if grid[target[0]][target[1]] > score:
+            # print ("score: %.1f" %  scoregrid[self.grid_size[1]-1-target[1]][target[0]])
+            if scoregrid[self.grid_size[1]-1-target[1]][target[0]] > score:
                 max_move = move
-                score = grid[target[0]][target[1]]
+                score = scoregrid[self.grid_size[1]-1-target[1]][target[0]]
     return max_move
 
 
